46418194a1c42f3e4a3b389af050b4fbbd47e30c
wanghuanle <wanghuanle@qq.com>
2020-10-22 13:33:07 +0800

    项目初步搭建、配置全局导航和底部导航

---
+++ pages/index/index.vue
@@ -0,0 +1,90 @@
+<template>
+	<view>
+		<nav-bar>
+			<text slot="left" class="font-md ml-3">首页</text>
+			<template slot="right">
+				<view style="width: 60rpx;height: 60rpx;" class="flex align-center justify-center bg-icon rounded-circle mr-3"><text class="iconfont icon-zengjia"></text></view>
+				<view style="width: 60rpx;height: 60rpx;" class="flex align-center justify-center bg-icon rounded-circle mr-3"><text class="iconfont icon-gengduo"></text></view>
+			</template>
+		</nav-bar>
+		 <view class="bg-info" style="height: 1000px;">		 
+		         <!-- 搜索框，设置圆角 -->
+		        <uni-search-bar :radius='100'></uni-search-bar>
+		    </view>
+			 <text v-for="(item,index) in list" :key="item.id">{{item.name}}{{item.price}},</text>
+	</view>
+</template>
+<script>
+import uniSearchBar from '@/components/common/search/uni-search-bar/uni-search-bar.vue'
+import navBar from '@/components/common/nav-bar.vue';
+export default {
+	data() {
+		return {
+			title: 'index',
+			list: [
+			        // {
+			        //   type: 'dir',
+			        //   name: '我的笔记',
+			        //   create_time: '2020-10-21 08:00',
+			        //   checked: false
+			        // },
+			        // {
+			        //   type: 'image',
+			        //   name: '风景.jpg',
+			        //   create_time: '2020-10-21 08:00',
+			        //   checked: false
+			        // },
+			        // {
+			        //   type: 'video',
+			        //   name: 'uniapp实战教程.mp4',
+			        //   create_time: '2020-10-21 08:00',
+			        //   checked: false
+			        // },
+			        // {
+			        //   type: 'text',
+			        //   name: '记事本.txt',
+			        //   create_time: '2020-10-21 08:00',
+			        //   checked: false
+			        // },
+			        // {
+			        //   type: 'none',
+			        //   name: '压缩包.rar',
+			        //   create_time: '2020-10-21 08:00',
+			        //   checked: false
+			        // }
+			      ]
+		};
+	},	
+	
+	onLoad() {
+		this.getShare()
+		
+	},
+	 methods: {
+	    getShare(){
+	      uni.request({
+	        url: 'http://127.0.0.1:7001/list',
+	        method: 'GET',
+	        success: res => {
+	          this.list = res.data.data;
+	          console.log(this.list)
+	        },
+	        fail: function(err) {
+	          uni.showToast({
+	            title: '请求失败'
+	          });
+	        },
+	        
+	      })
+	    },
+	  },
+	components:{
+		navBar,
+		uniSearchBar
+	}
+};
+</script>
+
+<style>
+
+</style>
---
+++ pages/my/my.vue
@@ -0,0 +1,71 @@
+<template>
+	<view>
+		<view class="flex align-center justify-around m-2" style="height: 160rpx;">
+			<view style="width: 160rpx;height: 160rpx;"><image src="../../static/logo.png" style="width: 160rpx;height: 160rpx;"></image></view>
+			<view class="flex-column flex-5 m-2">
+				<view><text class="font-md font-weight-bold">昵称</text></view>
+				<view class="flex">
+					<text class="text-secondary font-sm mr-2">总帖子10</text>
+					<text class="text-secondary font-sm">今日发帖0</text>
+				</view>
+			</view>
+			<view><text class="font-sm">></text></view>
+		</view>
+			<view class="flex align-center px-3 py-2">
+				<view class="flex-1 flex flex-column align-center justify-center" v-for="(item,index) in myDemo" :key=index>
+				<text class="font-lg font-weight-bold">{{item.num}}</text>
+				<text class="font text-muted">{{item.name}}</text>
+					</view>
+					</view>
+		<view class="m-3" style="height: 160rpx;"><image src="../../static/logo.png" style="height: 160rpx;width: 100%; border-radius: 3%;"></image></view>
+		<view class="flex flex-column" style="height: 360rpx;">
+			<uni-list-item title="浏览历史" showExtraIcon><text slot="icon" class="iconfont icon-liulan"></text></uni-list-item>
+			<uni-list-item title="社区认证" showExtraIcon><text slot="icon" class="iconfont icon-huiyuanvip"></text></uni-list-item>
+			<uni-list-item title="审核帖子" showExtraIcon><text slot="icon" class="iconfont icon-keyboard"></text></uni-list-item>
+		</view>
+	</view>
+</template>
+
+
+
+<script>
+	import uniListItem from '@/components/uni-ui/uni-ui/uni-list-item/uni-list-item.vue'
+	const demo = [
+		{
+			name: '动态',
+			num:'12'
+		},
+		{
+			name: '动态',
+			num:'12'
+		},
+		{
+			name: '动态',
+			num:'12'
+		},
+		{
+			name: '动态',
+			num:'12'
+		},
+	]
+	export default {
+		data() {
+			return {
+				myDemo:[]
+			}
+		},
+		onLoad() {
+			this.myDemo = demo;
+		},
+		methods: {
+			
+		},
+		components:{
+			uniListItem
+		}
+	}
+</script>
+
+
+
+<style></style>
