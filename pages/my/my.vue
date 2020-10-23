d7b424cb564dc1c6a03e9005ec9925ac2451f192
wanghuanle <wanghuanle@qq.com>
2020-10-23 14:34:05 +0800

    个人中心页面完成

--- pages/my/my.vue
+++ pages/my/my.vue
@@ -1,19 +1,50 @@
 <template>
-	<view>
-		
-	</view>
+  <view>
+    <view class="p-3 flex align-center">
+      <image
+        src="/static/1.jpg"
+        style="width: 120rpx;height: 120rpx;"
+        class="rounded-circle flex-shrink mr-3"
+      ></image>
+      <view class="flex-1 flex flex-column text-muted font">
+        <view class="flex align-end">
+          <text class="font-lg text-dark mr-2">Sunny</text>
+          女 江苏
+        </view>
+        <text class="text-ellipsis">软件工程师</text>
+      </view>
+    </view>
+    <view class="bg-light" style="height: 20rpx;"></view>
+    <view class="p-3">
+      <progress class="mb-3" percent="40" active stroke-width="3" />
+      <view class="flex align-center justify-between font">
+        <text class="text-light-muted">总：100GB</text>
+        <text class="text-warning">已用：80GB</text>
+      </view>
+    </view>
+    <view class="bg-light" style="height: 20rpx;"></view>
+    <view class="flex justify-between p-3">
+      <text class="text-muted font">设置</text>
+    </view>
+	<view class="flex justify-between p-3">   
+		 <text @click="loginout">退出登录</text>
+	    </view>
+  </view>
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-				
-			};
-		}
-	}
+export default {
+  data() {
+    return {};
+  },
+  methods: {
+	  loginout(){
+		  uni.navigateTo({
+		  	  url: '../login/login'
+		  });
+	  }
+  }
+};
 </script>
 
-<style lang="scss">
-
-</style>
+<style></style>
